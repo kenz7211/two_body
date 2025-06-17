dc1fb69229c004182bb3029a4217d1bf266460b3
Mackenzie Hayduk <mackenziehayduk2007@gmail.com>
2025-06-17 19:07:02 -0300

    Updates June 17th, 2025

--- two_body_utils.py
+++ two_body_utils.py
@@ -21,7 +21,7 @@ n_int = 10000
 M_SUN = const.M_sun.to(u.kg)
 
 # Masses of orbiting bodies in solar masses
-m_primary = 8200
+m_primary = 50
 m_secondary = 1
 
 # Distance to Omega Centauri center (km)
@@ -47,8 +47,8 @@ mtot = m_primary
 angular_distance_rad = (0.5*u.arcsec).to(u.rad).value
 semi_major_sample = angular_distance_rad*distance_km
 
-#Semi-major axis from Mark Gieles:
-#semi_major_sample = (750*u.au).to(u.km)
+# Other semi-major axis speculations:
+semi_major_sample = (200*u.au).to(u.km)
 
 
 # semi major axis used in IMBH_av_plots in kilometers
